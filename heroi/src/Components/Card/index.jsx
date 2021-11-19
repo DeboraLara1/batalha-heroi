be4888653a9d04d2444346aea0908fdfeaba7fb5
Debora Lara <debora.roldanlara@gmail.com>
2021-11-19 03:08:25 -0300

    Implementado filtro de busca

--- heroi/src/Components/Card/index.jsx
+++ heroi/src/Components/Card/index.jsx
@@ -1,32 +1,19 @@
 /* eslint-disable react/jsx-pascal-case */
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useState, useEffect } from 'react';
-import ListAPI from './../../Service/Api-MetaHumanos/MetaHumano.jsx';
+import React from 'react';
 import Styled from './Styled-Card';
 
-export default () => {
-    const [listUser, setListUser] = useState([]);
-
-    useEffect(() => {
-        const listInitial = async () => {
-            const list = await ListAPI.getList();
-            setListUser(list);
-        };
-        listInitial()
-        
-    }, []);
-
-    console.log(listUser)
-
+export default ({dataHeroList}) => {
+    
   return (
     <Styled.Section>
       <Styled.Row>
-        {listUser.map((getLis) => (
-            <Styled.metaHumanoRow key={getLis.id} >
+        {dataHeroList.map((getLisHero) => (
+            <Styled.metaHumanoRow key={getLisHero.id} >
               <div className="geral-moviesInitial">
                 <Styled.ContainerImg>
-                <img src={getLis.images.sm} alt={`Imagem do Meta Humano ${getLis.name}`} />
-                <Styled.H3 key={getLis.id}>{ getLis.name }</Styled.H3>
+                <img src={getLisHero.images.sm} alt={`Imagem do Meta Humano ${getLisHero.name}`} />
+                <Styled.H3 key={getLisHero.id}>{ getLisHero.name }</Styled.H3>
                 </Styled.ContainerImg>
               </div>
             </Styled.metaHumanoRow>
--- heroi/src/Pages/Pages-Initial/index.jsx
+++ heroi/src/Pages/Pages-Initial/index.jsx
@@ -1,22 +1,47 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import Styled from './Styled.jsx'
 
 import Card from '../../Components/Card'
+import ListAPI from './../../Service/Api-MetaHumanos/MetaHumano.jsx';
 
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+    const [filtro, setFiltro] = useState('')
 
-   
+    const [listHero, setListHero] = useState([]);
+
+    useEffect(() => {
+        const listInitial = async () => {
+            const list = await ListAPI.getList();
+            setListHero(list);
+        };
+        listInitial()
+        
+    }, []);
+
+    const handleChange = (event) => {
+        setFiltro(event.target.value);
+    };
+
+    const filtroHero = listHero.filter((hero) => {
+        return hero.name.toLowerCase().includes(filtro.toLowerCase())
+    })
+    
     return(
         
         <Styled.Contaneir>
             <Styled.Header>
-            <Styled.Search  type="shearch" placeholder="Busca" />
+            <Styled.Search  
+                type="text" 
+                value={filtro} 
+                onChange={handleChange} 
+                placeholder="Busca" 
+            />
             </Styled.Header>
             <Styled.Main>
-                <Card />
+                <Card dataHeroList={filtroHero} />
             </Styled.Main>
-            
         </Styled.Contaneir>
         
     )
